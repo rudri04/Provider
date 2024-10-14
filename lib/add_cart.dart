73b1caeb42d455f4a331a583573ac9c596918e0e
rudri04 <therudripatel@gmail.com>
2024-10-14 11:09:44 +0530

    added project with ui

--- lib/add_cart.dart
+++ lib/add_cart.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:provider_shoppinglist/Data/dummy_data.dart';
 import 'package:provider_shoppinglist/product_page.dart';
+import 'package:provider_shoppinglist/provider/const_provider.dart';
+import 'package:provider/provider.dart';
 class AddCart extends StatelessWidget {
   const AddCart({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CartProvider cartProvider = Provider.of<CartProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping List'),
@@ -15,6 +19,30 @@ class AddCart extends StatelessWidget {
           }, icon: const Icon(Icons.shopping_cart))
         ],
       ),
+      body: ListView.builder(
+        itemCount: productlist.length,
+          itemBuilder: (context,index){
+          product item = productlist[index];
+            return ListTile(
+              leading: Container(
+                height: 25,
+                width: 25,
+                color: item.color,
+              ),
+              title: Text(item.name),
+              trailing: Checkbox(
+                value: cartProvider.item.contains(item),
+                onChanged: (value) {
+                  if(value == true){
+                    cartProvider.add(item);
+                  }
+                  else {
+                    cartProvider.remove(item);
+                  }
+                },
+              ),
+            );
+      }),
     );
   }
 }
--- lib/product_page.dart
+++ lib/product_page.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_shoppinglist/Data/dummy_data.dart';
+import 'package:provider_shoppinglist/provider/const_provider.dart';
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Selected Products'),
       ),
+      body:Consumer<CartProvider>(builder: (context,provider,_){
+        return  Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * .8,
+              child: ListView.builder(
+                itemCount: provider.item.length,
+                  itemBuilder: (context,index){
+                    product pro = provider.item[index];
+                  return ListTile(
+                    title: Text(pro.name),
+                    onLongPress: (){
+                      provider.remove(pro);
+                    },
+                  );
+              }),
+            ),
+            Text('Total Cost Rs. ${provider.totalItems()}')
+          ],
+        );
+      })
     );
   }
 }
